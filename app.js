ab90f162293c845fb64118dae2a02df6d0267259
MuhammadAbbasweb-x04 <mabbasmshahi2003@gmail.com>
2025-10-26 08:44:52 -0700

    40done

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 // console.log("hell world");
 
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js"; 
-
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js"; 
+import { db } from "./config.js";
 const form = document.querySelector("#form");
 const todoTitle = document.querySelector("#todo-title");
 const todoDesc = document.querySelector("#todo-desc");
+let allTodo =[];
 
-
-const userData = {
-    title : todoTitle.value,
-    todoDesc : todoDesc.value
-}
 
 
 form.addEventListener("submit" ,async(e)=>{
     e.preventDefault();
- try {
-  const docRef = await addDoc(collection(db, userData), {
-   
-  })
+    const userData = {
+    title : todoTitle.value,
+    todoDesc : todoDesc.value
+}
+
+try {
+  const docRef = await addDoc(collection(db, "todos") ,userData)
   console.log("Document written with ID: ", docRef.id);
+  allTodo.push({...userData , docId: docRef.id})
 } catch (e) {
-  console.log("Error adding document: ", e);
+  console.error("Error adding document: ", e);
 }
 })
+
+
+function renderTodo(arr){
+
+}
